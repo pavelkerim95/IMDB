e7e1d4195d95bee09302883dba290bcec47e54a4
pavelkerim95 <pavelkerim95@gmail.com>
2026-01-21 14:33:28 +0100

    Add MovieRepository interface

---
+++ src/se/uu/imdb/data/MovieRepository.java
@@ -0,0 +1,23 @@
+package se.uu.imdb.data;
+import se.uu.imdb.model.Movie;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * Repository abstraction for movie persistence
+ */
+public interface MovieRepository {
+    /**
+     * Loads all movies from storage
+     * @return list of movies
+     * @throws IOException if reading fails
+     */
+    List<Movie> loadAll() throws IOException;
+
+    /**
+     * Saves all movies to storage
+      * @param movies
+     * @throws IOException
+     */
+    void saveAll(List<Movie> movies) throws IOException;
+}
